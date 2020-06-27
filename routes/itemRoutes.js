8ac44452150f7c2a7fc9680f1a24e71ea222e5ea
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-27 04:25:28 -0600

    Use hasOne in sequelize to join Item with Potion model in Potion get route.

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -7,8 +7,12 @@ const Op = Sequelize.Op;
 const itemRouter = express.Router();
 
 itemRouter.get('/potions', (req, res) => {
-  console.log('potions GET route reached...')
-  Potion.findAll()
+  console.log('potions GET route reached...');
+  Potion.hasOne(Item);
+  Potion.findAll({
+    include: 
+      { model: Item }
+  })
     .then(potion => {
       console.log(potion)
       res.status(200).send(potion)})
