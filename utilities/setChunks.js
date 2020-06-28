5f7bc7588d276948d364ab29575a568c4bbd99b7
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-28 12:51:57 -0600

    Modify filter in setChunks to look only for files that end in .js.

--- utilities/setChunks.js
+++ utilities/setChunks.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 let jsDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'js'));
-let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)/ig);});
+let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)$/ig);});
+console.log(jsChunks);
 
 let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'css'));
 let cssChunk = cssDir.filter((cssFile) => { return cssFile.match(/.*\.(css)/ig)});
