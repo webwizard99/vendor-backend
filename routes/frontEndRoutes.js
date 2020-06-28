c53ddf737723a2e7fb85ae57204712a5457d9969
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-28 10:58:07 -0600

    Fix express require in frontendRouter and load index.hbs from frontendRouter.

--- routes/frontEndRoutes.js
+++ routes/frontEndRoutes.js
@@ -1,5 +1,19 @@
-const express = require('express-handlebars');
+const express = require('express');
+const reactChunks = require('../config/reactChunks');
 
 const frontendRouter = express.Router();
+
+
+frontendRouter.get('/*', (req, res) => {
+  const chunk1 = reactChunks.path + reactChunks.chunk1;
+  const chunk2 = reactChunks.path + reactChunks.chunk2;
+  const css = reactChunks.cssPath + reactChunks.css;
+
+  res.render('index', {
+    script1: chunk1,
+    script2: chunk2,
+    css: css
+  });
+});
 
 module.exports = frontendRouter;
