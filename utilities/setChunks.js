e8f646bc88f40786a8093261557e5e81cf0b089f
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-28 12:57:15 -0600

    Further modify setChunks filter to exclude runtime file.

--- utilities/setChunks.js
+++ utilities/setChunks.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 let jsDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'js'));
 let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)$/ig);});
+jsChunks = jsChunks.filter((jsfile) => { return jsFile.match(/^[^r].+/ig);});
 console.log(jsChunks);
 
 let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'css'));
 let cssChunk = cssDir.filter((cssFile) => { return cssFile.match(/.*\.(css)/ig)});
 
 reactChunks.chunk1 = jsChunks[0];
-reactChunks.chunk2 = jsChunks[3];
+reactChunks.chunk2 = jsChunks[1];
 reactChunks.css = cssChunk[0];
