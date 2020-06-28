d71f47d8473bf51b033e458ded71cfffa424344f
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-28 10:50:07 -0600

    Add front end build folder.

---
+++ public/build/precache-manifest.66e447eaecc2181e2a4089d914bd205e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22aee726d097d5b64c4a9548b0cd194b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5f44c8fc19e76c4fae",
+    "url": "/static/css/main.0eef6633.chunk.css"
+  },
+  {
+    "revision": "916f0961fe694e2534f3",
+    "url": "/static/js/2.44b9007f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.44b9007f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5f44c8fc19e76c4fae",
+    "url": "/static/js/main.704f5928.chunk.js"
+  },
+  {
+    "revision": "fbfb76c737a5791c8f52",
+    "url": "/static/js/runtime-main.e903acfd.js"
+  }
+]);
