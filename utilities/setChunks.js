b4d0bf97f141890d496a276f8973c1b1ca615ba5
Michael Bragg <michael_bragg99@hotmail.com>
2020-06-28 13:04:07 -0600

    Fix typo in setChunks.

--- utilities/setChunks.js
+++ utilities/setChunks.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 let jsDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'js'));
 let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)$/ig);});
-jsChunks = jsChunks.filter((jsfile) => { return jsFile.match(/^[^r].+/ig);});
+jsChunks = jsChunks.filter((jsFile) => { return jsFile.match(/^[^r].+/ig);});
 console.log(jsChunks);
 
 let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'css'));
