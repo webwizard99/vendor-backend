540cf11cd441c4d4069fba294a52f819b97fed00
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-01 06:37:43 -0600

    Update backend.

--- utilities/setChunks.js
+++ utilities/setChunks.js
@@ -6,7 +6,6 @@ const path = require('path');
 let jsDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'js'));
 let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)$/ig);});
 jsChunks = jsChunks.filter((jsFile) => { return jsFile.match(/^[^r].+/ig);});
-console.log(jsChunks);
 
 let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'build', 'static', 'css'));
 let cssChunk = cssDir.filter((cssFile) => { return cssFile.match(/.*\.(css)/ig)});
