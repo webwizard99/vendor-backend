a5e57b884d9e0f0d296f9ba31a49eef9a0207b17
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-07 08:54:22 -0600

    Rever scoping in setEditorChunks.

--- utilities/setEditorChunks.js
+++ utilities/setEditorChunks.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 
-let jsDir = fs.readdirSync(path.join(__dirname, '../', '../', 'public', 'editor', 'build', 'static', 'js'));
+let jsDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'editor', 'build', 'static', 'js'));
 let jsChunks = jsDir.filter((jsFile) => { return jsFile.match(/.*\.(js)$/ig);});
 jsChunks = jsChunks.filter((jsFile) => { return jsFile.match(/^[^r].+/ig);});
 
-let cssDir = fs.readdirSync(path.join(__dirname, '../', '../', 'public', 'editor', 'build', 'static', 'css'));
+let cssDir = fs.readdirSync(path.join(__dirname, '../', 'public', 'editor', 'build', 'static', 'css'));
 let cssChunk = cssDir.filter((cssFile) => { return cssFile.match(/.*\.(css)/ig)});
 
 reactChunks.chunk1 = jsChunks[0];
