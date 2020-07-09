e073bab8357f54c10146e1fa9d15278d86c75bd3
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-09 04:38:55 -0600

    Modify routing to editor folder.

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'editor')));
+// app.use(express.static(path.join(__dirname, 'public', 'editor')));
 
 // mount routers
 app.use('/', itemRoutes);
 app.use('/', frontendRoutes);
 app.use('/', authRoutes);
-app.use('/editor', editorRoutes);
+app.use('/editor', express.static(path.join(__dirname, 'public', 'editor')), editorRoutes);
 
 app.listen(PORT);
