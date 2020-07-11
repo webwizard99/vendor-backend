f843ab6406bb95a04f5755929ebce0f97d4b00dd
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-11 05:05:35 -0600

    Attempt to reroute from editor route if user is not an owner type.

--- routes/editorRoutes.js
+++ routes/editorRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const editorReactChunks = require('../config/editorReactChunks');
 
+const userTypes = require('../config/userTypes');
+
 const editorRouter = express.Router();
 
 editorRouter.get('/', (req, res) => {
-  console.log(req.user);
+  console.log(req.user.dataValues);
+  if (req.user.dataValues.type !== userTypes.owner) {
+    res.redirect('/');
+  }
   const editorPrepend = '';
   const chunk1 = editorPrepend + editorReactChunks.path + editorReactChunks.chunk1;
   const chunk2 = editorPrepend + editorReactChunks.path + editorReactChunks.chunk2;
