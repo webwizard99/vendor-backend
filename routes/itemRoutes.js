86f660b2c84a5c07294e822a2bc1c6924f4fd253
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-19 11:56:49 -0600

    Add a potion post route.

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,6 +4,9 @@ const Potion = require('../models/Potion');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
+const itemTypes = require('../config/itemTypes');
+const potionTypes = require('../config/potionTypes');
+
 const itemRouter = express.Router();
 
 itemRouter.get('/potions', (req, res) => {
@@ -17,5 +20,68 @@ itemRouter.get('/potions', (req, res) => {
       res.status(200).send(potion)})
     .catch(err => console.log(err));
 });
+
+itemRouter.post('/potions', (req, res) => {
+  let {
+    key,
+    name,
+    value,
+    details,
+    type,
+    level
+
+  } = req.body;
+
+  if (key != process.env.EDITOR_API_KEY) {
+    res.status(404).send();
+  }
+
+  // validate input types
+  if (name && typeof name !== 'string') {
+    name = name.toString();
+  }
+  if (value && typeof value !== 'number') {
+    value = Number.parseInt(value);
+  }
+  if (details && typeof details !== 'string') {
+    details = details.toString();
+  }
+  if (type && typeof type !== 'string') {
+    type = type.toString();
+  }
+  if (level && typeof level !== 'number') {
+    level = Number.parseInt(level);
+  }
+
+  // reject request if missing a field
+  if (!name || !value || !type || !level || !potionTypes[type]) {
+    res.status(400).send();
+  } else {
+    try {
+        const newItem = await Item.create({
+        name,
+        type: itemTypes.potion,
+        value,
+        details
+      });
+    } catch(err) {
+      console.log(err);
+    }
+
+    const newItemId = newItem.id;
+    try {
+      const newPotion = await Potion.create({
+        itemId: newItemId,
+        type,
+        level
+      });
+    } catch(err) {
+      console.log(err);
+    }
+
+    res.status(200).send(newPotion);
+    
+  }
+})
 
 module.exports = itemRouter;
