083c6e72670674f85673e653a49e910e778ced9c
webwizard99 <michael_bragg99@hotmail.com>
2020-07-20 21:37:25 +0000

    Update editor React build files from dispatch event originating from frontend push.

--- public/editor/build/precache-manifest.45544a08933e2da4d4aa1d266aba7c5b.js
+++ public/editor/build/precache-manifest.45544a08933e2da4d4aa1d266aba7c5b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ed9933d7e5ac4dcf07c136a678e0397",
+    "revision": "f13ad051b826e3c1768d7a122f9eb801",
     "url": "/index.html"
   },
   {
-    "revision": "7a976d4ed5cbe254bad6",
+    "revision": "01915b407fb8120ad828",
     "url": "/static/css/main.94c7b3db.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.21d16b73.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7a976d4ed5cbe254bad6",
-    "url": "/static/js/main.6dc3f608.chunk.js"
+    "revision": "01915b407fb8120ad828",
+    "url": "/static/js/main.7801d5e4.chunk.js"
   },
   {
     "revision": "2dfae3a6133c6b654b97",
--- public/editor/build/service-worker.js
+++ public/editor/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae84928d1f15f568372833f5cc545d57.js"
+  "/precache-manifest.45544a08933e2da4d4aa1d266aba7c5b.js"
 );
 
 self.addEventListener('message', (event) => {
