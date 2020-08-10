b3ac34f7b3ed831e4f72ff32923bd506ff012372
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-10 16:58:52 -0600

    Add GET route to weaponsRouter.

--- routes/items/itemRoutes.js
+++ routes/items/itemRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 // require item type routers
 const potionRoutes = require('./potionRoutes');
+const weaponsRoutes = require('./weaponRoutes');
 
 const itemRouter = express.Router();
 
 // mount item type routes
 itemRouter.use('/', potionRoutes);
+itemRouter.use('/', weaponsRoutes);
 
 module.exports = itemRouter;
--- routes/items/weaponRoutes.js
+++ routes/items/weaponRoutes.js
@@ -6,3 +6,24 @@ const itemTypes = require('../../config/itemTypes');
 const authorization = require('../../middleware/authorization');
 
 const weaponRouter = express.Router();
+
+weaponRouter.get('/weapons', async (req, res) => {
+  console.log('weapons GET route reached...');
+
+  let allWeapons;
+  try {
+    Weapon.belongsTo(Item);
+    allWeapons = await Weapon.findAll({
+      include:
+        { model: Item }
+    })
+  } catch(err) {
+    console.log(err);
+  }
+  
+  res.status(200).send(allWeapons);
+  
+})
+
+
+module.exports = weaponRouter;
