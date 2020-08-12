07b4220c628b49acb5eb3c49d8b8617561dfc538
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-12 12:35:08 -0600

    Attempt different signature for group by query in suppliers GET route.

--- routes/suppliers/supplierRoutes.js
+++ routes/suppliers/supplierRoutes.js
@@ -17,7 +17,7 @@ supplierRouter.get('/suppliers', async (req, res) => {
       include: {
         model: Supplier
       },
-      group: 'supplierId',
+      group: [ { model: Supplier, as: 'Suppler'}, 'id'],
       // order: [
       //   [ { model: Supplier, as: 'Supplier'}, 'id' ]
       // ]
