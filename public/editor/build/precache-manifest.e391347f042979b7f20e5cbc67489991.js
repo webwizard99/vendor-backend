34a4251b73ba29e8ff0569ace159ea733558a292
webwizard99 <michael_bragg99@hotmail.com>
2020-08-15 23:31:39 +0000

    Update editor React build files from dispatch event originating from frontend push.

--- public/editor/build/precache-manifest.e391347f042979b7f20e5cbc67489991.js
+++ public/editor/build/precache-manifest.e391347f042979b7f20e5cbc67489991.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf88001477a4fc2e5cef34fb900c43f6",
+    "revision": "c9159f74cd898016fb6673dc9d87e3d0",
     "url": "/index.html"
   },
   {
-    "revision": "95eda3a2bae3e10c822e",
+    "revision": "3ed5154b0d2ba966794d",
     "url": "/static/css/main.acbfaad6.chunk.css"
   },
   {
-    "revision": "dabd0f1c21969ad0869a",
-    "url": "/static/js/2.9ea578ce.chunk.js"
+    "revision": "59b2c7a559f5f70c5531",
+    "url": "/static/js/2.5a575952.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.9ea578ce.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.5a575952.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "95eda3a2bae3e10c822e",
-    "url": "/static/js/main.d0f31f64.chunk.js"
+    "revision": "3ed5154b0d2ba966794d",
+    "url": "/static/js/main.d747d1be.chunk.js"
   },
   {
     "revision": "2dfae3a6133c6b654b97",
--- public/editor/build/service-worker.js
+++ public/editor/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dd3f453d983efcbd91b4fddd853f2eb.js"
+  "/precache-manifest.e391347f042979b7f20e5cbc67489991.js"
 );
 
 self.addEventListener('message', (event) => {
