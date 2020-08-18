f2df45171b896d1a79902286e655882d20b96314
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-17 18:31:29 -0600

    Add supplier PUT route.

--- routes/suppliers/supplierRoutes.js
+++ routes/suppliers/supplierRoutes.js
@@ -98,21 +98,145 @@ supplierRouter.post('/supplier', authorization, async (req, res) => {
       console.log(err);
       res.status(400);
     }
-
-    // Promise.allSettled(newOffering)
-    //   .then(async (result) => {
-
-    //   });
-
-    // try {
-    //   newSupplier.addOffering(newOffering);
-    // } catch(err) {
-    //   console.log(err);
-    //   res.status(400);
-    // }
   }
 
   res.status(200).send(true);
-})
+});
+
+supplierRouter.put('supplier', authorization, async(req, res) => {
+  let {
+    name,
+    existingIds,
+    deletedIds,
+    newIndexes
+  } = req.body;
+
+  // validate input types
+  if (name && typeof name !== 'string') {
+    name = name.toString();
+  }
+
+  // reject request if missing a field
+  if (!name) {
+    console.log('supplier post request missing field');
+    res.status(400).send();
+    return false;
+  }
+
+  if (typeof existingIds === 'string') {
+    existingIds = existingIds.split(',');
+  }
+  if (typeof deletedIds === 'string' && deletedIds !== '') {
+    deletedIds = deletedIds.split(',');
+  }
+  if (typeof newIndexes === 'string' && newIndexes !== '') {
+    newIndexes = newIndexes.split(',');
+  }
+
+  // associate models
+  Supplier.hasMany(Offering);
+  Offering.belongsTo(Supplier);
+
+  // update supplier model
+  let updatedSupplier;
+  try {
+    updatedSupplier = await Supplier.update({
+      name: name
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(false);
+    return;
+  }
+  
+  // handle existing offerings to be deleted
+  if (deletedIds.length > 0) {
+    for (let delId of deletedIds) {
+      // remove any ids to be deleted from existing list
+      if (existingIds.include(delId)) {
+        let delIndex = existingIds.indexOf(delId);
+        existingIds.splice(delIndex);
+      }
+  
+      // delete offering from database
+      try {
+        Offering.destroy({ where: { id: delId }});
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  
+  // handle existing offerings
+  if (existingIds.length > 0) {
+    for (let existId of existingIds) {
+      let offeringType = req.body[`offering-${existId}-type`];
+      let offeringMarkup = req.body[`offering-${existId}-markup`];
+  
+      // validate inputs
+      if (offeringType && typeof offeringType !== 'string') {
+        offeringType = offeringType.toString();
+      }
+      if (offeringMarkup && typeof offeringMarkup !== 'number') {
+        offeringMarkup = Number.parseInt(offeringMarkup);
+      }
+      if (!offeringType || !offeringMarkup) {
+        console.log('attempted to update offering with missing field');
+        res.status(400).send();
+      }
+  
+      // update model
+      try {
+        Offering.update({
+          type: offeringType,
+          markup: offeringMarkup
+        }, { where : {
+          id: existId
+        }});
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  
+  // If no offerings sent with supplier PUT request, finish request
+  if (newIndexes.length <= 0) {
+    res.status(200).send(true);
+    return;
+  }
+
+  // attempt to add all new offerings sent with PUT request
+  for (let index of newIndexes) {
+    let offeringType = req.body[`new-offering-${index}-type`];
+    let offeringMarkup = req.body[`new-offering-${index}-markup`];
+
+    // validate inputs
+    if (offeringType && typeof offeringType !== 'string') {
+      offeringType = offeringType.toString();
+    }
+    if (offeringMarkup && typeof offeringMarkup !== 'number') {
+      offeringMarkup = Number.parseInt(offeringMarkup);
+    }
+    if (!offeringType || !offeringMarkup) {
+      console.log('attempted to create offering with missing field');
+      res.status(400).send();
+    }
+
+    let newOffering;
+    try {
+      newOffering = await Offering.create({
+        type: offeringType,
+        markup: offeringMarkup,
+        supplierId: newSupplier.id
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400);
+    }
+  }
+
+  res.status(200).send(true);
+
+});
 
 module.exports = supplierRouter;
