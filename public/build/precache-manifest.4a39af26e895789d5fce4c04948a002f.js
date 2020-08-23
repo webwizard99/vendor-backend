46532499933fb0e2cca149ad5fe5841ceab5a8ba
webwizard99 <michael_bragg99@hotmail.com>
2020-08-23 00:32:36 +0000

    Update React build files from dispatch event originating from frontend push.

---
+++ public/build/precache-manifest.4a39af26e895789d5fce4c04948a002f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89990f6027285f196f36656800316ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b85ee08465760081ab19",
+    "url": "/static/css/main.0f5a2c7b.chunk.css"
+  },
+  {
+    "revision": "1b2003b00b986e1ab90d",
+    "url": "/static/js/2.28b87ba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28b87ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b85ee08465760081ab19",
+    "url": "/static/js/main.ca1bcd61.chunk.js"
+  },
+  {
+    "revision": "fbfb76c737a5791c8f52",
+    "url": "/static/js/runtime-main.e903acfd.js"
+  }
+]);
--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e84a78267011e8263fe279b7419e4473.js"
+  "/precache-manifest.4a39af26e895789d5fce4c04948a002f.js"
 );
 
 self.addEventListener('message', (event) => {
