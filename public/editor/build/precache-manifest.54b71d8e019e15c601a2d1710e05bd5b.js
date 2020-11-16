5ba476fadb1a6855e8975629e5b5720550d4e844
webwizard99 <michael_bragg99@hotmail.com>
2020-11-16 13:46:16 +0000

    Update editor React build files from dispatch event originating from frontend push.

--- public/editor/build/precache-manifest.54b71d8e019e15c601a2d1710e05bd5b.js
+++ public/editor/build/precache-manifest.54b71d8e019e15c601a2d1710e05bd5b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5ce1bb0a0c3b117bf3327ad4d09a1d2",
+    "revision": "a3da426d9391e4122ccf00cf06fee2fc",
     "url": "/index.html"
   },
   {
-    "revision": "0bf235b6b90ffd7fc4f1",
+    "revision": "26655cba144e64cc12ae",
     "url": "/static/css/main.dedb5759.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.527a19a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0bf235b6b90ffd7fc4f1",
-    "url": "/static/js/main.209d9fae.chunk.js"
+    "revision": "26655cba144e64cc12ae",
+    "url": "/static/js/main.4e483035.chunk.js"
   },
   {
     "revision": "2dfae3a6133c6b654b97",
