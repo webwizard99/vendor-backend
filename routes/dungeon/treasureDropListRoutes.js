afa4f1e321b32dba5bd81f4898c26e52a4f8939b
Michael Bragg <michael_bragg99@hotmail.com>
2020-12-09 05:38:57 -0700

    Add treasure drop lists GET route.

--- routes/dungeon/treasureDropListRoutes.js
+++ routes/dungeon/treasureDropListRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
+// model imports
+const TreasureDropList = require('../../models/TreasureDropList');
+const DropList = require('../../models/DropList');
+const Drop = require('../../models/Drop');
 // middleware imports
 const authorization = require('../../middleware/authorization');
 // utility imports
@@ -8,5 +11,28 @@ const validation = require('../../utilities/validation');
 const itemTypes = require('../../config/itemTypes');
 
 const treasureDropListRouter = express.Router();
+
+treasureDropListRouter.get('/treasure_drop_lists', async (req, res) => {
+  let treasureDropLists;
+  try {
+    DropList.hasOne(TreasureDropList);
+    TreasureDropList.belongsTo(DropList);
+    DropList.hasMany(Drop);
+    Drop.belongsTo(DropList);
+    treasureDropLists = await DropList.findAll({
+      include: [ {
+        model: TreasureDropList
+      },
+      { model: Drop}]
+    }, { where: {
+      type: dropListTypes.treasure
+    }});
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(false);
+    return;
+  }
+  res.status(200).send(treasureDropLists);
+})
 
 module.exports = treasureDropListRouter;
