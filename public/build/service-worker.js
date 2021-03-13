f0fc9d6434a4749877c1d38f7522f389f0b2cc9d
webwizard99 <michael_bragg99@hotmail.com>
2021-03-13 17:21:37 +0000

    Update React build files from dispatch event originating from frontend push.

--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81468cda0a80deecdcc126b6dfc1d4f9.js"
+  "/precache-manifest.3b38f574007b6dfdfb0336fc32dd655e.js"
 );
 
 self.addEventListener('message', (event) => {
